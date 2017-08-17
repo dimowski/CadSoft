531d4c339cd391216ebec950e5715ab099406791
Dmitry Kachurowski <dimaka4@gmail.com>
2017-08-17 21:04:56 +0300

    Create repository and entities

--- src/main/java/com/cadsoft/CadSoftApplication.java
+++ src/main/java/com/cadsoft/CadSoftApplication.java
@@ -1,10 +1,20 @@
 package com.cadsoft;
 
+import com.cadsoft.repository.ProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadSoftApplication {
+
+    @Autowired
+    private ProgramRepository repository;
+
+/*    @Bean
+    CommandLineRunner commandLineRunner() {
+        return (args) -> repository.save(new Program("Test", "Descr", "ShortDescr", new byte[50]));
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(CadSoftApplication.class, args);
---
+++ src/main/java/com/cadsoft/entity/Comment.java
@@ -0,0 +1,72 @@
+package com.cadsoft.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.ZonedDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String commenter;
+    private String ip;
+    private String content;
+    private ZonedDateTime date;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCommenter() {
+        return commenter;
+    }
+
+    public void setCommenter(String commenter) {
+        this.commenter = commenter;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", commenter='" + commenter + '\'' +
+                ", ip='" + ip + '\'' +
+                ", content='" + content + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
